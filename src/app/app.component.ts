21c64ee2bb37e766f3c964c42051034821a911b0
Aldo Santiago <aldo11santiago@gmail.com>
2022-07-11 11:58:27 -0500

    feat: aes example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'aes-app';
-}
+import { Component } from '@angular/core';  
+import * as CryptoJS from 'crypto-js';  
+  
+@Component({  
+  selector: 'app-root',  
+  templateUrl: './app.component.html',  
+  styleUrls: ['./app.component.css']  
+})  
+export class AppComponent {  
+  title = 'EncryptionDecryptionSample';  
+    
+  plainText:string = "";  
+  encryptText: string = "";  
+  encPassword: string = "";  
+  decPassword:string = "";  
+  operationMode:string = "";
+  conversionEncryptOutput: string = "";  
+  conversionDecryptOutput:string = "";  
+    
+  constructor() {  
+  }  
+  //method is used to encrypt and decrypt the text  
+  convertText(conversion:string) {  
+      if (conversion=="encrypt") {  
+        this.conversionEncryptOutput = CryptoJS.AES.encrypt(this.plainText.trim(), this.encPassword.trim()).toString();  
+      }  
+      else {  
+        this.conversionDecryptOutput = CryptoJS.AES.decrypt(this.encryptText.trim(), this.decPassword.trim()).toString(CryptoJS.enc.Utf8);  
+       
+    }  
+  }  
+}  
